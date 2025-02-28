8478b98166ef5d1584c630509276ae8259d3c74f
Abdallah Elnakib <113241788+Abdallah-Elnakib@users.noreply.github.com>
2025-02-28 18:11:52 +0200

    Update authVerifyOtpController.ts

--- controllers/authVerifyOtpController.ts
+++ controllers/authVerifyOtpController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import { User } from '../models/userModel';
-import { Otp } from '../models/otpSchema'; // Assuming you have an OTP model
+import { Otp } from '../models/otpSchema'; 
 import mailSender from '../utils/mailSender';
 
 
